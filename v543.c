3bf6cc3c311cb237c6a7992c05449ced7c77b885
bienata <natasza.biecek@gmail.com>
2018-05-18 19:42:30 +0000

    nieco komentu

--- v543.c
+++ v543.c
@@ -5,7 +5,7 @@ kompilacja:
   
 uruchomienie:
   ./v543
-
+  
 */
 
 #include <stdio.h>
@@ -30,12 +30,6 @@ uruchomienie:
 #define LED_READY   4
 #define LED_SCPI    5
 
-unsigned char   uchM2Led = 0;
-unsigned long   ulRawMeterData = 0L;
-unsigned char   uchRangeId = 0;
-unsigned char   uchModeId = 0;
-unsigned char   uchPolarity = 0;
-
 char* trim(char*);  // proto, kod niżej
 void handlerIdn(char*);
 void handlerRst(char*); 
@@ -47,7 +41,6 @@ void handlerDisplay(char*);
 void handlerExit(char*); 
 
 
-
 // prototyp handlerka komendy scpi, po prostu wypełnia wynik w out i tyle
 typedef void (*TScpiCommandHandler)(char*);
 
@@ -57,13 +50,22 @@ typedef struct {
     TScpiCommandHandler handler;
 } TCommand;
 
-
+// na informacje o zakresie
 typedef struct {
     const char  *label;
     int scale;
 } TRangeInfo;
 
 
+unsigned char   uchLedReady = 0;
+unsigned char   uchLedScpi = 0;
+unsigned long   ulRawMeterData = 0L;
+unsigned char   uchRangeId = 0;
+unsigned char   uchModeId = 0;
+unsigned char   uchPolarity = 0;
+
+
+// to póki co obsługujemy
 TCommand scpiCommands[] = {
     {   "*idn?",              &handlerIdn },    
     {   ":meter:mode?",       &handlerMode },
@@ -113,6 +115,7 @@ void makeLower ( char *s ) {
     }   
 }
 
+// identyfikacja urządzenia 
 void handlerIdn(char *out) {    
     strcpy ( out, "Meratronik V543 No.01473, SCPI connector, tasza (c) 2018\n");
 }
@@ -132,6 +135,7 @@ void handlerRRange(char *out) {
     sprintf( out, "%d|%s|%d\n", uchRangeId, resRanges[ uchRangeId ].label, resRanges[ uchRangeId ].scale );            
 }
 
+// odsyła znak i pięć cyferek wyświetlacza
 void handlerDisplay(char *out) {
     char sign = ' ';
     if ( uchModeId == 4 /*DC*/){
@@ -139,28 +143,33 @@ void handlerDisplay(char *out) {
     }
     sprintf( out, "%c%05X\n", sign, ulRawMeterData &0x1FFFF );    
 } 
-    
+
+// 32 bity hex
 void handlerRawData(char *out) {
     sprintf( out, "%08X\n", ulRawMeterData );    
 }
 
+// debugowe wyjście
 void handlerExit(char *out) {
     strcpy( out, "exit here\n" );    
 }
 
 
+// rozpoznanie i wykoannie polecenia SCPI
 int processScpiCommand ( char *cmd, char *out ) {
     int o = 0;
     // domyslnie blad
-    strcpy ( out, "error\n" );
-    
+    strcpy ( out, "error\n" );    
     for( int i = 0; scpiCommands[i].cmd != NULL; ++i ) {
         if ( strcmp ( cmd, scpiCommands[i].cmd ) == 0 ) {
             // trafiony!
             (scpiCommands[i].handler)(out);
             o = (scpiCommands[i].handler == handlerExit);
+            break;
         }
     }
+    digitalWrite ( LED_READY, uchLedScpi ) ;        
+    uchLedScpi ^= 1;    
     return o;
 }
 
@@ -183,14 +192,16 @@ unsigned long readV543rawData( void ) {
 }
 
 
-
+// obsługa przerwania od GPIO z pinu LINE_READY Meratronika
 void onMeterReadyInterrupt( void ) {        
+    // fizyczny odczyt
     ulRawMeterData = readV543rawData();    
     uchRangeId = (ulRawMeterData >> 17) & 0x07;
     uchModeId = (ulRawMeterData >> 22) & 0x07;
     uchPolarity = (ulRawMeterData >> 20) & 0x03;
-    digitalWrite ( LED_READY, uchM2Led ) ;        
-    uchM2Led ^= 1;
+    // mignięcie ledem
+    digitalWrite ( LED_READY, uchLedReady ) ;        
+    uchLedReady ^= 1;
 }
 
 // main foo.
@@ -232,7 +243,7 @@ int main( int argc, char *argv[] ) {
      
      if ( bind( serverSocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr) ) < 0 ) {
          perror ( "01 error when binding server socket" );
-	 exit (1);
+	     exit (1);
      }
      
      listen( serverSocket, 5);     
@@ -244,39 +255,41 @@ int main( int argc, char *argv[] ) {
         printf( "10 waiting for connection\n" );
 	
         if ( ( clientSocket = accept( serverSocket, (struct sockaddr *) &cli_addr, (socklen_t*) &clilen) ) < 0 ) {
-	    perror ( "02 error on accept incoming connection" );
-	    exit (1);
-	}
+	        perror ( "02 error on accept incoming connection" );
+	        exit (1);
+	    }
 	
         printf( "11 waiting for input data\n" );
 	
-	bzero( commandBuffer, sizeof( commandBuffer ) );
-	bzero( responseBuffer, sizeof( responseBuffer ) );	  	
+	    bzero( commandBuffer, sizeof( commandBuffer ) );
+	    bzero( responseBuffer, sizeof( responseBuffer ) );	  	
 	
-	if ( ( n = read( clientSocket, commandBuffer, sizeof( commandBuffer ) - 1 ) ) < 0 ){
-	  perror ( "03 error when receiving request" );
-	  exit (1);	      
-	}
+    	if ( ( n = read( clientSocket, commandBuffer, sizeof( commandBuffer ) - 1 ) ) < 0 ){
+	        perror ( "03 error when receiving request" );
+	        exit (1);	      
+	    }
 
-    makeLower( commandBuffer );
-	int exitRequest = processScpiCommand ( trim( commandBuffer ), responseBuffer );
+        makeLower( commandBuffer );
+        
+	    int exitRequest = processScpiCommand ( trim( commandBuffer ), responseBuffer );
     
-	if ( (n = write( clientSocket, responseBuffer, strlen( responseBuffer ) ) ) < 0 ) {
-	  perror ( "04 error when sending response" );
-	  exit (1);	      	      	   
-	} 
-	else {
-        printf( "12 SCPI [%s]->[%s]\n", commandBuffer, trim( responseBuffer)  );    
-    }
-	
-	// release conn to client
+	    if ( (n = write( clientSocket, responseBuffer, strlen( responseBuffer ) ) ) < 0 ) {
+	        perror ( "04 error when sending response" );
+	        exit (1);	      	      	   
+	    } 
+	    else {
+            printf( "12 SCPI [%s]->[%s]\n", commandBuffer, trim( responseBuffer)  );    
+        }
+
+	    // release conn to client
         close( clientSocket );
         
 	    if ( exitRequest ) {        
             printf( "19 done\n" );                
             break;
         }
-     } // of while
+        
+     } // of while     
      
      return 0; 
 }
